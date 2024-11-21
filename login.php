76d6ab17eeb9ce9bba38fc1960782c47fa729dfa
m.szcz <gnpmapper@gmail.com>
2024-11-21 13:34:26 +0100

    21.11.2024 commit 6

--- login.php
+++ login.php
@@ -1,20 +1,30 @@
 <?php 
 include_once('polaczenie.php');
 session_start();
-$imie = $_POST['imie'];
+
+$email = $_POST['email'];
 $haslo = $_POST['haslo'];
 
-$zapytania = 'SELECT * FROM users WHERE imie = "'.$imie.'" AND haslo = "'.$haslo.'"';
-
-$wynik = $polaczenie->query($zapytania);
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo 'Nieprawidłowy format email';
+    exit();
+}
+if (strlen($haslo) < 8 || !preg_match('/[0-9]/', $haslo)) {
+    echo 'Hasło musi mieć minimum 8 znaków i zawierać przynajmniej jedną cyfrę';
+    exit();
+}
+$zapytanie = $polaczenie->prepare('SELECT * FROM users WHERE email = ? AND haslo = ?');
+$zapytanie->bind_param('ss', $email, $haslo);
+$zapytanie->execute();
+$wynik = $zapytanie->get_result();
 
 if($wynik->num_rows > 0){
     $_SESSION['zalogowany'] = true;
-    $_SESSION['imie'] = $imie;
+    $user = $wynik->fetch_assoc();
+    $_SESSION['imie'] = $user['imie'];
     header('Location: index.php');
 }
 else {
-    echo 'nie zalogowano';
+    echo 'Nieprawidłowy email lub hasło';
 }
-
 ?>
--- logowanie.php
+++ logowanie.php
@@ -8,13 +8,15 @@
 <body>
     
     <section class="main">
-        <form action="login.php" method="post">
-            <label for="imie">Imię: </label>
-            <input type="text" name="imie">
-            <label for="haslo">Haslo: </label>
-            <input type="password" name="haslo">
+        <form action="login.php" method="post" id="logowanie">
+            <label for="email">Email: </label>
+            <input type="email" name="email" id="email" required>
+            <label for="haslo">Hasło: </label>
+            <input type="password" name="haslo" id="haslo" minlength="8" required>
+            <div class="info">Hasło musi mieć minimum 8 znaków i zawierać przynajmniej jedną cyfrę</div>
             <input type="submit" value="Zaloguj">
         </form>
     </section>
+    <script src="zabezpieczenia.js"></script>
 </body>
 </html>

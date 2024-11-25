66e1cd6494b2a04912f7a5ecbdd7b4e29b44bb85
m.szcz <gnpmapper@gmail.com>
2024-11-25 14:19:48 +0100

    "Updated PHP files (dodaj.php, index.php, logout.php) and HTML file (logowanie.html) with changes to form elements, styles, and layout. Removed style.css file."

--- index.php
+++ index.php
@@ -8,23 +8,142 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="styl.css">
+    <style>
+        * {
+            box-sizing: border-box;
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        }
+        body {
+            display: flex;
+            flex-direction: column;
+            height: 100vh;
+            width:100vw;
+            margin: 0;
+            padding: 0;
+            background-color: #f5f5f5;
+        }
+        .main {
+            display: flex;
+            flex-direction: row;
+            width:100%;
+            height:95%;
+        }
+        .header {
+            height: 5%;
+        }
+        .headerWrapper {
+            display: flex;
+            margin-top: 20px;
+        }
+        .header a {
+            text-decoration: none;
+            color: black;
+            background-color:#1a6283; 
+            border: 0px;
+            padding: 5px;
+            border-radius: 10px;
+        }
+        .wylog {
+            margin-left: 800px;
+        }
+        .imie {
+            margin-right: 800px;
+        }
+        .hero {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+        }
+        .heroWrapper {
+            
+        }
+        form *{
+            font-size: 20px;
+        }
+        form div {
+            display: flex;
+            flex-direction: column;
+        }
+        .header * {
+            font-size: 20px; 
+        }
+        .wybor {
+            display: flex;
+            flex-direction: row;
+        }
+        .wybor * {
+            margin-left: 12px;
+            display: flex;
+            flex-direction: row;
+        }
+        .wybor * input {
+            display: flex;
+            flex-direction: row;
+            margin-top: 10px;
+            width: 20px;
+            height:20px;
+        } 
+        input {
+            padding: 5px;
+            margin: 5px;
+            width: 450px;
+        }
+        .sprowadzony {
+            flex-direction: row;
+        }
+        .sprowadzony * input{
+            width: 220px;
+        } 
+        .zdjecie #zdjecie{
+            width: 370px;
+            margin-left: 80px;
+        }
+        label {
+           text-align: center;
+        }
+        input[list] {
+            width: 450px;
+        }
+        form {
+            margin: 20px;
+        }
+        #technologiaContainer {
+            display: block;
+        }
+        #dodajProdukt {
+            background-color:#2cb3e9;
+            border: 0px;
+            border-radius: 25px;
+            height: 55px;
+            width: 280px;
+            margin-left: 80px;
+        }
+        #pokazZdjecie {
+
+        }
+    </style> 
 </head>
 <body>
-    <header>
-    <?php 
+    <section class="header">
+        <?php 
+        //  <section class="sidebar"></section>
         if((!isset($_SESSION['zalogowany']))){
             echo '<a href="logowanie.html">Zaloguj się</a>';
         }
         else {
-            echo '<div class=imie>'.$_SESSION['imie'].'</div>';
-            echo '<a href="logout.php">Wyloguj</a>';
+            echo '
+            <div class="headerWrapper">
+            <div class="imie">Uzytkownik: '.$_SESSION['imie'].'</div> 
+            <a class="wylog" href="logout.php">Wyloguj się</a>
+            </div>' ;
         
-    ?>
-    </header>
-        <section class="main">
-            <?php 
-           echo' <form action="dodaj.php" method="post" enctype="multipart/form-data">
+    ?></section>
+    <section class="main">
+        <section class="hero">
+            <div class="heroWrapper">
+        <?php echo' <form action="dodaj.php" method="post" enctype="multipart/form-data">
                 <div class="nazwaContainer">
                     <label for="nazwaProd">Nazwa produktu: </label>
                     <input list="nazwyProd" name="nazwaProd" id="nazwaProd" placeholder="nazwa produktu">
@@ -70,50 +189,39 @@ session_start();
                 </div>
                 <div class="sprowadzony" id="sprowadzony">
                     <div class="cenaOryginalna">
-                        <label for="cenaOryginalna">Cena oryginalna: </label>
+                        <label for="cenaOryginalna">Cena oryginalna(zł): </label>
                         <input type="text" name="cenaOryginalna" id="cenaOryginalna" placeholder="0.00">
-                        <label for="cenaOryginalna">zł</label>
                     </div>
                     <div class="marza">
-                        <label for="marza">Marza: </label>
+                        <label for="marza">Marza(%): </label>
                         <input type="text" name="marza" id="marza" placeholder="0">
-                        <label for="marza">%</label>
                     </div>
                 </div>
-                <div class="cena" id="cena">
-                    <label for="cena">Cena: </label>
-                    <input type="text" name="cena" id="cena" placeholder="0.00">
-                    <label for="cena">zł</label>
+                <div class="cena" id="cena" style="display: flex;">
+                    <label for="cenaId">Cena(zł): </label>
+                    <input type="text" name="cena" id="cenaId" placeholder="0.00">
                 </div>
                 <div class="zdjecie">
-                    <label for="zdjecie">Zdjęcie: </label>
+                    <label for="zdjecie">Zdjęcie(maksymalnie 64KB!): </label>
                     <input type="file" name="zdjecie" id="zdjecie" accept="image/gif, image/jpeg, image/png">
-                    <label for="zdjecie">(maksymalnie 64kb!)</label>
-                    <div id="pokazZdjecie"></div>
                 </div>
                 <input type="submit" value="Dodaj produkt" id="dodajProdukt">
-            </form>
-            <div id="podsumowanie">
-                <h2>Podsumowanie</h2>
-                <div class="podsumowanieContainer">
-                    <div id="cena_produktu"></div>
-                    <div id="koszt_przygotowania"></div>
-                    <div id="cena_nadruku"></div>
-                    <div id="cena_jednostkowa"></div>
-                    <div id="marza"></div>
-                </div>
-            </div> '
-;}?>
+            </form>';}
+            ?>
+            </div>
+            <div id="pokazZdjecie">Tu pojawi się obraz</div>
         </section>
-        <script src="podsumowanie.js"></script>
+        </section>
         <script>
             getNazwyProd(); toggleTechnologia(); toggleIloscKolorow(); getKolory(); toggleOff('sprowadzony'); toggleOff('cena');
             function wyswietlZdjecie(tablicaZdj) {
                 let zdjecia = ""
                 tablicaZdj.forEach((zdj) => {
-                zdjecia += `<img src="${URL.createObjectURL(zdj)}" alt="image" height="25%" width="25%">`;
+                zdjecia += `<img src="${URL.createObjectURL(zdj)}" alt="image" height="50%" width="50%">`;
                 });
+                document.getElementById('pokazZdjecie').innerHTML = '';
                 document.getElementById('pokazZdjecie').innerHTML = zdjecia;
+
             }
             document.getElementById('zdjecie').addEventListener('change', function() {
                 let tablicaZdj = [];
@@ -125,7 +233,7 @@ session_start();
                 document.getElementById($ktoryOff).style.display = 'none';
             }
             function toggleOn($ktoryOn) {
-                document.getElementById($ktoryOn).style.display = 'block';
+                document.getElementById($ktoryOn).style.display = 'flex';
             }
             document.getElementById('cudzy').addEventListener('click', function() {
                 if(document.getElementById('wlasny').checked) {
@@ -180,7 +288,7 @@ session_start();
                     techContainer.style.display = 'none';
                     document.getElementById('iloscKolorowContainer').value = '';
                 } else {
-                    techContainer.style.display = 'block';
+                    techContainer.style.display = 'flex';
                 }
             }
             function toggleIloscKolorow() {
@@ -190,7 +298,7 @@ session_start();
                 if (techInput.value === 'technologia znakowania' || techInput.value === '') {
                     iloscKolorowContainer.style.display = 'none';
                 } else {
-                    iloscKolorowContainer.style.display = 'block';
+                    iloscKolorowContainer.style.display = 'flex';
                 }
             } 
             function getKolory() {
